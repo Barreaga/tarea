e201c9a80fdd954c1600c057ac2c9df6838db1e0
Barreaga <124693720+Barreaga@users.noreply.github.com>
2023-08-05 14:40:21 -0600

    Primer commit

---
+++ src/ejercicio02/Ejercicio02.java
@@ -0,0 +1,28 @@
+
+package ejercicio02;
+
+import java.util.Scanner;
+
+
+public class Ejercicio02 {
+
+ 
+    public static void main(String[] args) {
+        
+	{
+		Scanner a = new Scanner(System.in);
+		int n1;
+		System.out.print("Ingrese el numero: ");
+		n1=a.nextInt();
+                
+                if(n1 >= 0)
+                    System.out.println("Es Positivo");
+		else
+                    System.out.println("Es Negativo");
+        
+                
+        }  
+        
+    }
+   
+}
