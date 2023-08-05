5776940c29a44c84cca6557737b28ff7f98ef22b
Brandon Arreaga <124693720+Barreaga@users.noreply.github.com>
2023-08-05 15:19:29 -0600

    Add files via upload

---
+++ src/ejercicio05/Ejercicio05.java
@@ -0,0 +1,13 @@
+
+package ejercicio05;
+
+import java.util.Scanner;
+
+public class Ejercicio05 {
+   
+    public static void main(String[] args) {
+     
+        Scanner a = new Scanner(System.in);
+    }
+    
+}
